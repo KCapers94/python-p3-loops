f4b88616925e338fc049815e0e0cd0db2d544d1a
Keith Capers <K.Capers94@gmail.com>
2024-09-24 21:11:24 -0400

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,27 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i >= 1:
+       print(i)
+       i -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared = [x ** 2 for x in int_list]
+    return squared
+    
 
 def fizzbuzz():
     # code goes here!
-    pass
+   for num in range(1,101):     
+    if num % 15 == 0:
+        print("FizzBuzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    else:
+       print(num)
